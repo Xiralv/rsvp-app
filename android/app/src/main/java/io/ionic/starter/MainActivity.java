59f3cb54b4996b37c6e0e25f414e952476738e81
Vlarix <vlarix@Vlarixs-MacBook-Pro.local>
2025-07-23 16:47:15 +0800

    Deploy Ionic to GitHub Pages

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package io.ionic.rsvp;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- src/app/services/supabase/supabase.service.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const SUPABASE_URL = 'https://ctthrwffzswlkmxoudet.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN0dGhyd2ZmenN3bGtteG91ZGV0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMzM3NDksImV4cCI6MjA2NTkwOTc0OX0.AsAxk4KmgxagygVnBPX-fvOmtd2ueCB5MYaqrsphSHM';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
